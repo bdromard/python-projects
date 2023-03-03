2e176c3111812ff0224db9de17eaf60066100b0e
bdromard <benjamin.dromard@gmail.com>
2023-03-03 16:54:04 +0100

    sauvegarde

--- day-20-snake/scoreboard.py
+++ day-20-snake/scoreboard.py
@@ -3,11 +3,15 @@ ALIGNMENT = 'center'
 FONT = ('Courier', 24, 'normal')
 
 
+
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("score.txt", mode="r") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.color('white')
         self.penup()
@@ -15,13 +19,19 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f'Score = {self.score}', False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f'Score = {self.score} High Score: {self.high_score}', False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write('GAME OVER', False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            # With keyword opens file and closes it when we are done with the program. Executes written-down functions.
+            with open("score.txt", mode="w") as file:
+                file.write(str(self.high_score))
+
+        self.score = 0
+        self.update_scoreboard()
